59b1103a66e050d33a987953f1c9f1eff02b3f98
Emilio Vanni <info@emiliovanni.com>
2020-04-12 22:46:43 +0100

    multiple sprites and basic collision

--- test/integration/toasters.py
+++ test/integration/toasters.py
@@ -34,90 +34,280 @@ dims = (display.width, display.height)
 # Animation Variables
 shift = 10
 
-def toastersprite(randomstart):
-    start = randomstart
-    hor = start
+# def toastersprite(randomstart):
+#     start = randomstart
+#     hor = start
+#     ver = -100
+#     delta = 200
+#     while hor > -220:
+#
+#         img1.thumbnail(dims)
+#         paste_coords = hor, ver
+#         display.frame_buf.paste(img1, paste_coords)
+#         display.draw_partial(constants.DisplayModes.DU)
+#
+#         #second_toaster
+#         img1.thumbnail(dims)
+#         paste_coords = hor-delta, ver-delta
+#         display.frame_buf.paste(img1, paste_coords)
+#         display.draw_partial(constants.DisplayModes.DU)
+#
+#         hor = hor - shift
+#         ver = ver + shift
+#         print(f'toaster position {hor},{ver}')
+#
+#         img2.thumbnail(dims)
+#         paste_coords = hor, ver
+#         display.frame_buf.paste(img2, paste_coords)
+#         display.draw_partial(constants.DisplayModes.DU)
+#
+#         #second_toaster
+#         img2.thumbnail(dims)
+#         paste_coords = hor-delta, ver-delta
+#         display.frame_buf.paste(img2, paste_coords)
+#         display.draw_partial(constants.DisplayModes.DU)
+#
+#         hor = hor - shift
+#         ver = ver + shift
+#
+#         img3.thumbnail(dims)
+#         paste_coords = hor, ver
+#         display.frame_buf.paste(img3, paste_coords)
+#         display.draw_partial(constants.DisplayModes.DU)
+#
+#         #second_toaster
+#         img3.thumbnail(dims)
+#         paste_coords = hor-delta, ver-delta
+#         display.frame_buf.paste(img3, paste_coords)
+#         display.draw_partial(constants.DisplayModes.DU)
+#
+#         hor = hor - shift
+#         ver = ver + shift
+#
+#         img4.thumbnail(dims)
+#         paste_coords = hor, ver
+#         display.frame_buf.paste(img4, paste_coords)
+#         display.draw_partial(constants.DisplayModes.DU)
+#
+#         #second_toaster
+#         img4.thumbnail(dims)
+#         paste_coords = hor-delta, ver-delta
+#         display.frame_buf.paste(img4, paste_coords)
+#         display.draw_partial(constants.DisplayModes.DU)
+#
+#         hor = hor - shift
+#         ver = ver + shift
+#
+#         img3.thumbnail(dims)
+#         paste_coords = hor, ver
+#         display.frame_buf.paste(img3, paste_coords)
+#         display.draw_partial(constants.DisplayModes.DU)
+#
+#         #second_toaster
+#         img3.thumbnail(dims)
+#         paste_coords = hor-delta, ver-delta
+#         display.frame_buf.paste(img3, paste_coords)
+#         display.draw_partial(constants.DisplayModes.DU)
+#
+#         hor = hor - shift
+#         ver = ver + shift
+#
+#         img2.thumbnail(dims)
+#         paste_coords = hor, ver
+#         display.frame_buf.paste(img2, paste_coords)
+#         display.draw_partial(constants.DisplayModes.DU)
+#
+#         #second_toaster
+#         img2.thumbnail(dims)
+#         paste_coords = hor-delta, ver-delta
+#         display.frame_buf.paste(img2, paste_coords)
+#         display.draw_partial(constants.DisplayModes.DU)
+#
+#         hor = hor - shift
+#         ver = ver + shift
+#
+# def toastsprite(randomstart):
+#     start = randomstart
+#     hor = start
+#     ver = -100
+#     while hor > -220:
+#
+#         toast.thumbnail(dims)
+#         paste_coords = hor, ver
+#         display.frame_buf.paste(toast, paste_coords)
+#         display.draw_partial(constants.DisplayModes.DU)
+#
+#         hor = hor - shift
+#         ver = ver + shift
+        
+def ensemble(randomstart):
+    start1 = randomstartoaster1
+    start2 = randomstartoaster2
+    start3 = randomstartoast
+    hor1 = start1
+    hor2 = start2
+    hor3 = start3
     ver = -100
-    hor2 = start -50
-    ver2 = -150
-    while hor > -220:
+    while hor1 > -220:
     
-       img1.thumbnail(dims)
-       paste_coords = hor, ver
-       display.frame_buf.paste(img1, paste_coords)
-       display.draw_partial(constants.DisplayModes.A2)
+        img1.thumbnail(dims)
+        paste_coords = hor1, ver
+        display.frame_buf.paste(img1, paste_coords)
+        display.draw_partial(constants.DisplayModes.DU)
        
-       img1.thumbnail(dims)
-       paste_coords = hor2, ver2
-       display.frame_buf.paste(img1, paste_coords)
-       display.draw_partial(constants.DisplayModes.A2)
-   
-       hor = hor - shift
-       ver = ver + shift
-       print(f'toaster position {hor},{ver}')
-    
-        img2.thumbnail(dims)
-        paste_coords = hor, ver
-        display.frame_buf.paste(img2, paste_coords)
+        #second_toaster
+        img1.thumbnail(dims)
+        paste_coords = hor2, ver - delta
+        display.frame_buf.paste(img1, paste_coords)
         display.draw_partial(constants.DisplayModes.DU)
-    
-        hor = hor - shift
-        ver = ver + shift
-    
-        img3.thumbnail(dims)
-        paste_coords = hor, ver
-        display.frame_buf.paste(img3, paste_coords)
-        display.draw_partial(constants.DisplayModes.DU)
-    
-        hor = hor - shift
-        ver = ver + shift
-    
-        img4.thumbnail(dims)
-        paste_coords = hor, ver
-        display.frame_buf.paste(img4, paste_coords)
-        display.draw_partial(constants.DisplayModes.DU)
-    
-        hor = hor - shift
-        ver = ver + shift
-    
-        img3.thumbnail(dims)
-        paste_coords = hor, ver
-        display.frame_buf.paste(img3, paste_coords)
-        display.draw_partial(constants.DisplayModes.DU)
-    
-        hor = hor - shift
-        ver = ver + shift
-    
-        img2.thumbnail(dims)
-        paste_coords = hor, ver
-        display.frame_buf.paste(img2, paste_coords)
-        display.draw_partial(constants.DisplayModes.DU)
-    
-        hor = hor - shift
-        ver = ver + shift
-
-def toastsprite(randomstart):
-    start = randomstart
-    hor = start
-    ver = -100
-    while hor > -220:
-    
+        
+        #toast
         toast.thumbnail(dims)
-        paste_coords = hor, ver
+        paste_coords = hor3, ver - toastdelta
         display.frame_buf.paste(toast, paste_coords)
         display.draw_partial(constants.DisplayModes.DU)
         
-        hor = hor - shift
+        hor1 = hor1 - shift
+        hor2 = hor2 - shift
+        hor3 = hor3 - shift
+        ver = ver + shift
+        
+        print(f'toaster1 position {hor1},{ver}')
+        print(f'toaster2 position {hor2},{ver}')
+        print(f'toast position {hor3},{ver}')
+    
+        img2.thumbnail(dims)
+        paste_coords = hor1, ver
+        display.frame_buf.paste(img2, paste_coords)
+        display.draw_partial(constants.DisplayModes.DU)
+        
+        #second_toaster
+        img2.thumbnail(dims)
+        paste_coords = hor2, ver - delta
+        display.frame_buf.paste(img2, paste_coords)
+        display.draw_partial(constants.DisplayModes.DU)
+        
+        #toast
+        toast.thumbnail(dims)
+        paste_coords = hor3, ver - toastdelta
+        display.frame_buf.paste(toast, paste_coords)
+        display.draw_partial(constants.DisplayModes.DU)
+    
+        hor1 = hor1 - shift
+        hor2 = hor2 - shift
+        hor3 = hor3 - shift
+        ver = ver + shift
+    
+        img3.thumbnail(dims)
+        paste_coords = hor1, ver
+        display.frame_buf.paste(img3, paste_coords)
+        display.draw_partial(constants.DisplayModes.DU)
+        
+        #second_toaster
+        img3.thumbnail(dims)
+        paste_coords = hor2, ver - delta
+        display.frame_buf.paste(img3, paste_coords)
+        display.draw_partial(constants.DisplayModes.DU)
+        
+        #toast
+        toast.thumbnail(dims)
+        paste_coords = hor3, ver - toastdelta
+        display.frame_buf.paste(toast, paste_coords)
+        display.draw_partial(constants.DisplayModes.DU)
+    
+        hor1 = hor1 - shift
+        hor2 = hor2 - shift
+        hor3 = hor3 - shift
+        ver = ver + shift
+    
+        img4.thumbnail(dims)
+        paste_coords = hor1, ver
+        display.frame_buf.paste(img4, paste_coords)
+        display.draw_partial(constants.DisplayModes.DU)
+        
+        #second_toaster
+        img4.thumbnail(dims)
+        paste_coords = hor2, ver - delta
+        display.frame_buf.paste(img4, paste_coords)
+        display.draw_partial(constants.DisplayModes.DU)
+        
+        #toast
+        toast.thumbnail(dims)
+        paste_coords = hor3, ver - toastdelta
+        display.frame_buf.paste(toast, paste_coords)
+        display.draw_partial(constants.DisplayModes.DU)
+    
+        hor1 = hor1 - shift
+        hor2 = hor2 - shift
+        hor3 = hor3 - shift
+        ver = ver + shift
+    
+        img3.thumbnail(dims)
+        paste_coords = hor1, ver
+        display.frame_buf.paste(img3, paste_coords)
+        display.draw_partial(constants.DisplayModes.DU)
+        
+        #second_toaster
+        img3.thumbnail(dims)
+        paste_coords = hor2, ver - delta
+        display.frame_buf.paste(img3, paste_coords)
+        display.draw_partial(constants.DisplayModes.DU)
+        
+        #toast
+        toast.thumbnail(dims)
+        paste_coords = hor3, ver - toastdelta
+        display.frame_buf.paste(toast, paste_coords)
+        display.draw_partial(constants.DisplayModes.DU)
+    
+        hor1 = hor1 - shift
+        hor2 = hor2 - shift
+        hor3 = hor3 - shift
+        ver = ver + shift
+    
+        img2.thumbnail(dims)
+        paste_coords = hor1, ver
+        display.frame_buf.paste(img2, paste_coords)
+        display.draw_partial(constants.DisplayModes.DU)
+        
+        #second_toaster
+        img2.thumbnail(dims)
+        paste_coords = hor2, ver - delta
+        display.frame_buf.paste(img2, paste_coords)
+        display.draw_partial(constants.DisplayModes.DU)
+        
+        #toast
+        toast.thumbnail(dims)
+        paste_coords = hor3, ver - toastdelta
+        display.frame_buf.paste(toast, paste_coords)
+        display.draw_partial(constants.DisplayModes.DU)
+    
+        hor1 = hor1 - shift
+        hor2 = hor2 - shift
+        hor3 = hor3 - shift
         ver = ver + shift
         
 # animating the display
 x = 1
 while True:
     # display.draw_full(constants.DisplayModes.DU)
-    randomstart = random.randint(1,801)
-    print(f'### toast starting position: {randomstart}')
-    toastsprite(randomstart)
-    
-    randomstartoaster = random.randint(1,801)
-    print(f'### toaster starting position: {randomstartoaster}')
-    toastersprite(randomstartoaster)
+    # randomstart = random.randint(1,801)
+#     print(f'### toast starting position: {randomstart}')
+#     toastsprite(randomstart)
+#
+#     randomstartoaster = random.randint(1,801)
+#     print(f'### toaster starting position: {randomstartoaster}')
+#     toastersprite(randomstartoaster)
+
+    randomstartoaster1 = random.randint(1,801)
+    print(f'### toaster1 starting position: {randomstartoaster1}')
+    randomstartoaster2 = random.randint(1,801)
+    if randomstartoaster2 in range(randomstartoaster1-164, randomstartoaster1+164):
+     print('### toaster2 collision ###')
+    print(f'### toaster2 starting position: {randomstartoaster2}')
+    randomstartoast = random.randint(1,801)
+    print(f'### toast starting position: {randomstartoast}')
+    toastdelta = random.randint(1,801)
+    print(f'### toast delta: {toastdelta}')
+    delta = random.randint(1,801)
+    print(f'### toaster delta: {delta}')
+    ensemble(randomstartoaster1)
